0427fed3f122b6586e919a50e15d64e1ec19ded5
Andrius Urbelis <andrius.urbelis@coxautoinc.com>
2022-08-02 16:38:04 +0300

    task6: refresh config

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://dy8pdouh49.execute-api.eu-central-1.amazonaws.com/dev',
-  bff: ' https://w0z0xq2ju8.execute-api.eu-central-1.amazonaws.com/dev',
+  import: 'https://th5b9up3e3.execute-api.eu-central-1.amazonaws.com/dev/',
+  bff: ' https://th5b9up3e3.execute-api.eu-central-1.amazonaws.com/dev/',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
