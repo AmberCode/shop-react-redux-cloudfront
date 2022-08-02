5fb062fd3f5a95bc140d61221432ca4d3d6559a8
Andrius Urbelis <andrius.urbelis@coxautoinc.com>
2022-08-02 17:23:53 +0300

    config

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://0h45bb8q6c.execute-api.eu-central-1.amazonaws.com/dev',
+  import: 'https://th5b9up3e3.execute-api.eu-central-1.amazonaws.com/dev',
   bff: 'https://0h45bb8q6c.execute-api.eu-central-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
