8d4292972f5f68e244afac0b7ace08dbb590ee75
ambercode <andrius.urbelis@gmail.com>
2022-08-17 17:59:06 +0300

    task8: cart url (#5)
    Co-authored-by: Andrius Urbelis <andrius.urbelis@coxautoinc.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://th5b9up3e3.execute-api.eu-central-1.amazonaws.com/dev',
   bff: 'https://0h45bb8q6c.execute-api.eu-central-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://ambercode-cart-api-dev.eu-central-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
